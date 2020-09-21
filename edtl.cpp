801184562e4028292e5ea91ce19eb74b945fbf05
Sergey <serg_soft@mail.ru>
2020-09-21 22:07:32 +0700

    Implemented new checker and 5 cases from the article

--- edtl.cpp
+++ edtl.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 #define DEBUG
 
@@ -7,12 +8,14 @@ enum vars { H, D };
 // test case:
 // HANDS yes/no
 int H_VAL[] = {1, 1, 0, 0, 0};
-// HANDS DRIER on/off
+// HANDS DRYER on/off
 int D_VAL[] = {0, 1, 1, 1, 1};
+// todo: test cases
 
+// logic in terms
 class Term {
  public:
-  virtual int value(int i, int j, int h) = 0;
+  virtual int value(int i, int j) = 0;
   void debug(const std::string &msg, int i) {
 #ifdef DEBUG
     std::cout << "value of " << i << msg << " term" << std::endl;
@@ -26,7 +29,7 @@ class ConstTerm : public Term {
 
  public:
   ConstTerm(int c) : val(c) {}
-  int value(int i, int j, int h) { return val; }
+  int value(int i __unused, int j __unused) { return val; }
 };
 
 // value of the variable
@@ -36,7 +39,7 @@ class ValTerm : public Term {
 
  public:
   ValTerm(vars var) : var(var) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j __unused) {
     if (i < 0) i = 0;
     debug("val", i);
 
@@ -55,10 +58,10 @@ class NegTerm : public Term {
 
  public:
   NegTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("neg", i);
 
-    return !term->value(i, j, h);
+    return !term->value(i, j);
   }
 };
 
@@ -69,10 +72,10 @@ class AndTerm : public Term {
 
  public:
   AndTerm(Term *term1, Term *term2) : term1(term1), term2(term2) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("and", i);
 
-    return term1->value(i, j, h) && term2->value(i, j, h);
+    return term1->value(i, j) && term2->value(i, j);
   }
 };
 
@@ -83,25 +86,25 @@ class OrTerm : public Term {
 
  public:
   OrTerm(Term *term1, Term *term2) : term1(term1), term2(term2) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("or", i);
 
-    return term1->value(i, j, h) || term2->value(i, j, h);
+    return term1->value(i, j) || term2->value(i, j);
   }
 };
 
-// our values
+// our addition
 class TildaTerm : public Term {
  private:
   Term *term;
 
  public:
   TildaTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("tilda", i);
     if (i <= 0) return false;
 
-    return (i != 0) && term->value(i - 1, j, h) && term->value(i, j, h);
+    return (i != 0) && term->value(i - 1, j) && term->value(i, j);
   }
 };
 
@@ -111,25 +114,25 @@ class SlashTerm : public Term {
 
  public:
   SlashTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("slash", i);
     if (i <= 0) return false;
 
-    return (i != 0) && term->value(i - 1, j, h) && !term->value(i, j, h);
+    return (i != 0) && term->value(i - 1, j) && !term->value(i, j);
   }
 };
 
-class BackShashTerm : public Term {
+class BackSlashTerm : public Term {
  private:
   Term *term;
 
  public:
-  BackShashTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  BackSlashTerm(Term *term) : term(term) {}
+  int value(int i, int j) {
     debug("backslash", i);
     if (i <= 0) return false;
 
-    return (i != 0) && !term->value(i - 1, j, h) && term->value(i, j, h);
+    return (i != 0) && !term->value(i - 1, j) && term->value(i, j);
   }
 };
 
@@ -139,11 +142,11 @@ class UnderlineTerm : public Term {
 
  public:
   UnderlineTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("underline", i);
     if (i <= 0) return false;
 
-    return (i != 0) && !term->value(i - 1, j, h) && !term->value(i, j, h);
+    return (i != 0) && !term->value(i - 1, j) && !term->value(i, j);
   }
 };
 
@@ -154,10 +157,10 @@ class PassedTerm : public Term {
 
  public:
   PassedTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("passed", i);
 
-    return (i * h >= j * h + term->value(i, j, h));
+    return (i >= j + term->value(i, j));  // check it
   }
 };
 
@@ -167,11 +170,11 @@ class ChangesTerm : public Term {
 
  public:
   ChangesTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("changes", i);
     if (i <= 0) return false;
 
-    return term->value(i - 1, j, h) != term->value(i, j, h);
+    return term->value(i - 1, j) != term->value(i, j);
   }
 };
 
@@ -181,11 +184,11 @@ class IncreasesTerm : public Term {
 
  public:
   IncreasesTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("increases", i);
     if (i <= 0) return false;
 
-    return term->value(i - 1, j, h) < term->value(i, j, h);
+    return term->value(i - 1, j) < term->value(i, j);
   }
 };
 
@@ -195,11 +198,11 @@ class DecreasesTerm : public Term {
 
  public:
   DecreasesTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("decreases", i);
     if (i <= 0) return false;
 
-    return term->value(i - 1, j, h) > term->value(i, j, h);
+    return term->value(i - 1, j) > term->value(i, j);
   }
 };
 
@@ -209,11 +212,11 @@ class NotIncreasesTerm : public Term {
 
  public:
   NotIncreasesTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("notincreases", i);
     if (i <= 0) return false;
 
-    return term->value(i - 1, j, h) <= term->value(i, j, h);
+    return term->value(i - 1, j) <= term->value(i, j);
   }
 };
 
@@ -223,126 +226,259 @@ class NotDecreasesTerm : public Term {
 
  public:
   NotDecreasesTerm(Term *term) : term(term) {}
-  int value(int i, int j, int h) {
+  int value(int i, int j) {
     debug("notdecreases", i);
     if (i <= 0) return false;
 
-    return term->value(i - 1, j, h) >= term->value(i, j, h);
+    return term->value(i - 1, j) >= term->value(i, j);
   }
 };
 
-// abstract class for the system
+// abstract class for the req
+class CheckableReq {
+ protected:
+  std::string desc;
+
+ public:
+  CheckableReq() {}
+
+  void setDesc(const std::string &desc) { this->desc = desc; }
+  std::string &getDesc() { return desc; }
+
+  virtual int trigger(int i, int j) = 0;
+  virtual int invariant(int i, int j) = 0;
+  virtual int reaction(int i, int j) = 0;
+  virtual int release(int i, int j) = 0;
+  virtual int delay(int i, int j) = 0;
+  virtual int final(int i, int j) = 0;
+
+  bool check(int len) {
+    int trig = 1;
+    while (trig < len) {
+      if (this->trigger(trig, 0)) {
+        if (this->release(trig, trig)) goto checked;
+        int fin = trig;
+        while (!this->final(fin, trig)) {
+          if (this->release(fin, trig)) goto checked;
+          if (!this->invariant(fin, trig)) return false;
+          fin++;
+          if (fin == len) goto checked;
+        }
+        int del = fin;
+        while (!this->delay(del, fin) && !this->reaction(del + 1, fin)) {
+          if (this->release(del, trig)) goto checked;
+          if (!this->invariant(del, fin)) return false;
+          del++;
+          if (del == len) goto checked;
+        }
+        if (!this->release(del, trig) && this->delay(del, fin) &&
+            !this->invariant(del, fin))
+          return false;
+      }
+    checked:
+      trig++;
+    }
+    return true;
+  }
+};
+
+class CASE1 : public CheckableReq {
+ public:
+  CASE1() {
+    setDesc(
+        "If the dryer is on, then it turns off after no hands for 1 second");
+  }
+
+  virtual int trigger(int i, int j) {  // \H && D
+    return (new AndTerm(new BackSlashTerm(new ValTerm(vars::H)),
+                        (new ValTerm(vars::D))))
+        ->value(i, j);
+  }
+
+  virtual int release(int i, int j) {  // H
+    return (new ValTerm(vars::H))->value(i, j);
+  }
+
+  virtual int final(int i, int j) {  // passed 1s
+    return (new PassedTerm(new ConstTerm(1)))->value(i, j);
+  }
+
+  virtual int delay(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int invariant(int i, int j) {  // D
+    return (new ValTerm(vars::D))->value(i, j);
+  }
+
+  virtual int reaction(int i, int j) {  // !D
+    return (new NegTerm(new ValTerm(vars::D)))->value(i, j);
+  }
+};
+
+class CASE2 : public CheckableReq {
+ public:
+  CASE2() {
+    setDesc(
+        "If the dryer was not turned on and hands appeared, it will turn on "
+        "after no more than 1 cycle");
+  }
+
+  virtual int trigger(int i, int j) {  // /H && !D
+    return (new AndTerm(new SlashTerm(new ValTerm(vars::H)),
+                        (new NegTerm(new ValTerm(vars::D)))))
+        ->value(i, j);
+  }
+
+  virtual int release(int i __unused, int j __unused) {  // false
+    return false;
+  }
+
+  virtual int final(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int delay(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int invariant(int i, int j) {  // !D
+    return (new NegTerm(new ValTerm(vars::D)))->value(i, j);
+  }
+
+  virtual int reaction(int i, int j) {  // D
+    return (new ValTerm(vars::D))->value(i, j);
+  }
+};
+
+class CASE3 : public CheckableReq {
+ public:
+  CASE3() {
+    setDesc("If there are hands and the dryer is on, it will not turn off");
+  }
+
+  virtual int trigger(int i, int j) {  // H && D
+    return (new AndTerm(new ValTerm(vars::H), new ValTerm(vars::D)))
+        ->value(i, j);
+  }
+
+  virtual int release(int i __unused, int j __unused) {  // false
+    return false;
+  }
+
+  virtual int final(int i __unused, int j __unused) {  // !H
+    return (new NegTerm(new ValTerm(vars::H)))->value(i, j);
+  }
+
+  virtual int delay(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int invariant(int i, int j) {  // D
+    return (new ValTerm(vars::D))->value(i, j);
+  }
+
+  virtual int reaction(int i __unused, int j __unused) {  // *
+    return true;
+  }
+};
+
+class CASE4 : public CheckableReq {
+ public:
+  CASE4() {
+    setDesc(
+        "If there are no hands and the dryer is not turned on, the dryer will "
+        "not turn on until the hands appear");
+  }
+
+  virtual int trigger(int i, int j) {  // !H && !D
+    return (new AndTerm(new NegTerm(new ValTerm(vars::H)),
+                        new NegTerm(new ValTerm(vars::D))))
+        ->value(i, j);
+  }
+
+  virtual int release(int i, int j) {  // H
+    return (new ValTerm(vars::H))->value(i, j);
+  }
+
+  virtual int final(int i __unused, int j __unused) {  // false
+    return false;
+  }
+
+  virtual int delay(int i __unused, int j __unused) {  // *
+    return true;
+  }
+
+  virtual int invariant(int i, int j) {  // !D
+    return (new NegTerm(new ValTerm(vars::D)))->value(i, j);
+  }
+
+  virtual int reaction(int i __unused, int j __unused) {  // *
+    return true;
+  }
+};
+
+class CASE5 : public CheckableReq {
+ public:
+  CASE5() { setDesc("The time of continuous work is no more than an hour"); }
+
+  virtual int trigger(int i, int j) {  // /D
+    return (new SlashTerm(new NegTerm(new ValTerm(vars::D))))->value(i, j);
+  }
+
+  virtual int release(int i, int j) {  // \D
+    return (new BackSlashTerm(new NegTerm(new ValTerm(vars::D))))->value(i, j);
+  }
+
+  virtual int final(int i __unused, int j __unused) {              // passed(1h)
+    return (new PassedTerm(new ConstTerm(60 * 60)))->value(i, j);  //??
+  }
+
+  virtual int delay(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int invariant(int i __unused, int j __unused) {  // true
+    return true;
+  }
+
+  virtual int reaction(int i, int j) {  // \D
+    return (new BackSlashTerm(new NegTerm(new ValTerm(vars::D))))->value(i, j);
+  }
+};
+
 class CheckableSystem {
  public:
-  CheckableSystem() {}
-  virtual int trigger(int i, int j, int h) = 0;
-  virtual bool isTrigger() = 0;
-  virtual int reset(int i, int j, int h) = 0;
-  virtual int postcondition(int i, int j, int h) = 0;
-  virtual int precondition(int i, int j, int h) = 0;
+  CheckableSystem(){};
 
-  virtual int ASAP(int i, int j, int h) = 0;
-  virtual int final(int i, int j, int h) = 0;
-
-  bool check(int h, int max) {
-    int i = 0;
-    while (true) {
-      if (i >= max) return true;
-      if (this->trigger(i, 0, h)) {
-        if (this->reset(i, 0, h)) goto checked;
-        int j = i;
-        while (this->final(j, i, h)) {
-          if (j >= max) return true;
-
-          if (this->reset(j, i, h)) goto checked;
-          if (this->precondition(j, i, h)) {
-            std::cout << "first case: ";
-            std::cout << i << j << std::endl;
-            return false;
-          }
-          j++;
-        }
-        int k = j;
-        while (this->ASAP(k, j, h)) {
-          if (k >= max) return true;
-
-          if (this->reset(k, j, h)) goto checked;
-          if (this->postcondition(k, j, h)) break;
-          if (!this->precondition(k, j, h)) {
-            std::cout << "second case: ";
-            std::cout << i << j << k << std::endl;
-            return false;
-          }
-          k++;
-        }
-      }
-    checked:;
-      i++;
+  void addReqs(std::initializer_list<CheckableReq *> reqs) {
+    for (auto req : reqs) {
+      addReq(req);
     }
   }
+  void addReq(CheckableReq *req) { reqs.push_back(req); }
+
+  bool check() {
+    int max = sizeof(H_VAL) / sizeof(int) - 1;
+
+    for (auto req : reqs) {
+      std::cout << "Checking '" << req->getDesc() << "'" << std::endl;
+      if (!req->check(max)) return false;
+    }
+    return true;
+  }
+
+ private:
+  std::list<CheckableReq *> reqs;
 };
 
-/*
-class CASE1 : public CheckableSystem {
- public:
-  virtual int trigger(int i, int j, int h) {
-    return (new AndTerm(new SlashTerm(new ValTerm(vars::H)),
-                        new TildaTerm(new ValTerm(vars::D))))
-        ->value(i, j, h);
-  };
+int main(int argc __unused, char *argv[] __unused) {
+  CheckableSystem *system = new CheckableSystem();
 
-  virtual bool isTrigger() { return true; }
-  virtual int reset(int i, int j, int h) {
-    return (new ValTerm(vars::H))->value(i, j, h);
-  }
-  virtual int postcondition(int i, int j, int h) {
-    return (new ValTerm(vars::D))->value(i, j, h);
-  }
-  virtual int precondition(int i, int j, int h) {
-    return (new ValTerm(vars::D))->value(i, j, h);
-  }
+  system->addReqs(
+      {new CASE1(), new CASE2(), new CASE3(), new CASE4(), new CASE5()});
 
-  virtual int ASAP(int i, int j, int h) { return 1; }
-  virtual int final(int i, int j, int h) { return 5; }
-};
-*/
-
-class CASE4 : public CheckableSystem {
- public:
-  virtual int trigger(int i, int j, int h) {
-    return (new AndTerm(new ValTerm(vars::H), new ValTerm(vars::D)))
-        ->value(i, j, h);
-  };
-
-  virtual bool isTrigger() { return true; }
-  virtual int reset(int i, int j, int h) { return false; }
-  virtual int postcondition(int i, int j, int h) {
-    std::cout << "postcondition called" << std::endl;
-    return (new NegTerm(new ValTerm(vars::D)))->value(i, j, h);
-  }
-  virtual int precondition(int i, int j, int h) {
-    std::cout << "precondition called" << std::endl;
-
-    return (new NegTerm(new ValTerm(vars::D)))->value(i, j, h);
-  }
-
-  virtual int ASAP(int i, int j, int h) {
-    return (new PassedTerm(new ConstTerm(0)))->value(i, j, h);
-  }
-  virtual int final(int i, int j, int h) {
-    int ret = (new PassedTerm(new ConstTerm(0)))->value(i, j, h);  // 0 was safe
-    std::cout << "final returned " << ret << std::endl;
-    return ret;
-  }
-};
-
-int main(int argc, char *argv[]) {
-  CheckableSystem *system = new CASE4();
-
-  int max = sizeof(H_VAL) / sizeof(int) - 1;
-
-  int h = 10;  //
-  if (system->check(h, max))
+  if (system->check())
     std::cout << "System is safe" << std::endl;
   else
     std::cout << "! System is unsafe " << std::endl;
